0a814aaea72ab17e819f513611d086daff917cbb
Chad W Pry <chad.pry@gmail.com>
2014-08-10 19:12:06 -0400

    remove log file in heroku

--- lib/mentor_match/application.rb
+++ lib/mentor_match/application.rb
@@ -13,9 +13,6 @@ module MentorMatch
       set :haml, format: :html5
       enable :show_exceptions
       enable :logging
-      file = File.new("#{settings.root}log/#{settings.environment}.log", 'a+')
-      file.sync
-      use Rack::CommonLogger, file
     end
 
     get '/' do
