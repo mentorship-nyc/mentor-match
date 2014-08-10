52f4f9dc7bc03a24c88dc0f5de867995a4056829
Chad W Pry <chad.pry@gmail.com>
2014-08-10 19:05:05 -0400

    update root path

--- lib/mentor_match/application.rb
+++ lib/mentor_match/application.rb
@@ -13,7 +13,7 @@ module MentorMatch
       set :haml, format: :html5
       enable :show_exceptions
       enable :logging
-      file = File.new("#{settings.root}/log/#{settings.environment}.log", 'a+')
+      file = File.new("#{settings.root}log/#{settings.environment}.log", 'a+')
       file.sync
       use Rack::CommonLogger, file
     end
