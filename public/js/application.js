e9cf781e0f7307f2382d168c190bb171df961f67
Chad W Pry <chad.pry@gmail.com>
2014-08-12 18:19:57 -0400

    update signup form dialog

--- public/js/application.js
+++ public/js/application.js
@@ -1,6 +1,23 @@
 (function() {
   $('.btn-signup').click(function() {
-    $('div.signup').slideToggle();
-    $('input#name').focus();
+    $slidebox = $('#slidebox');
+    current_margin = $slidebox.css('margin-top').substring(0, $slidebox.css('margin-top').length - 2);
+    if (current_margin > 0) {
+      margin = '-266px';
+    } else {
+      margin = '30px';
+    }
+
+    $slidebox.animate({
+      'margin-top': margin
+    }, function() {
+      $('input#name').focus();
+    });
+  });
+
+  $(document).ready(function() {
+    setTimeout(function() {
+      $('#slidebox').toggle();
+    }, 1000);
   });
 })();
