dddce0f319ad56de024e752d86a6cea57bc310da
Chad W Pry <chad.pry@gmail.com>
2014-08-12 03:41:38 -0400

    correct buggy config

--- lib/initializers/0003_pony.rb
+++ lib/initializers/0003_pony.rb
@@ -1,26 +1,22 @@
 require 'pony'
 
-if ENV['RACK_ENV'] == 'production'
-  options = {
-    from: 'noreply@mentoring-nyc.com',
-    via: :smtp,
-    via_options: {
-      address: 'smtp.mandrillapp.com',
-      port: '587',
-      user_name: 'app28354064@heroku.com',
-      password: 'vn0CxMD-lGCL35Fd1mKwGg',
-      authentication: :plain,
-      domain: 'mentoring-nyc.com'
-    }
+options = {
+  from: 'The Mentoring NYC Team <team@mentoring-nyc.com>',
+  via: :smtp,
+  via_options: {
+    address: 'localhost', port: 1025,
+    authentication: :plain, domain: 'localhost.localdomain'
   }
-else
-  options = {
-    from: 'noreply@mentoring-nyc.com',
-    via: :smtp,
-    via_options: {
-      address: 'localhost', port: 1025,
-      authentication: :plain, domain: 'localhost.localdomain'
-    }
+}
+
+if ENV['RACK_ENV'] == 'production'
+  options[:via_options] = {
+    address: 'smtp.mandrillapp.com',
+    port: '587',
+    user_name: 'app28354064@heroku.com',
+    password: 'vn0CxMD-lGCL35Fd1mKwGg',
+    authentication: :plain,
+    domain: 'mentoring-nyc.com'
   }
 end
 
