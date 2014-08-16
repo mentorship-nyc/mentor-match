5cd244efd671fe3304f041eba95f8ea7274a400a
Chad W Pry <chad.pry@gmail.com>
2014-08-16 19:52:21 -0400

    correct challenge field name

--- db/migrate/20140816214217_add_challenge_model.rb
+++ db/migrate/20140816214217_add_challenge_model.rb
@@ -3,7 +3,7 @@ class AddChallengeModel < ActiveRecord::Migration
     create_table :challenges do |t|
       t.string  :name,        limit: 100, null: false
       t.string  :description, limit: 255, null: false
-      t.string  :difficult,   limit: 10,  null: false
+      t.string  :difficulty,  limit: 10,  null: false
       t.text    :problem,                 null: false
 
       t.timestamps
